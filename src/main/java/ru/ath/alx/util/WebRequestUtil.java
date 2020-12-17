e411f9cccaf057e64245573b73ceba843d3d677b
alx787 <kaanova@gmail.com>
2020-12-17 16:55:16 +0300

    tune ssl

--- src/main/java/ru/ath/alx/util/WebRequestUtil.java
+++ src/main/java/ru/ath/alx/util/WebRequestUtil.java
@@ -31,30 +31,30 @@ public class WebRequestUtil {
         // вобщем это будет подключаться к ssl без проверки сертификата
         // не пойму почему не видит сертификат, хотя я его импортировал в хранилище
         /* Start of Fix */
-        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
-            public void checkClientTrusted(X509Certificate[] certs, String authType) { }
-            public void checkServerTrusted(X509Certificate[] certs, String authType) { }
-
-        } };
-
-        SSLContext sc = null;
-        try {
-            sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (KeyManagementException e) {
-            e.printStackTrace();
-        }
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-        // Create all-trusting host name verifier
-        HostnameVerifier allHostsValid = new HostnameVerifier() {
-            public boolean verify(String hostname, SSLSession session) { return true; }
-        };
-        // Install the all-trusting host verifier
-        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+//        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+//            public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
+//            public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+//            public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+//
+//        } };
+//
+//        SSLContext sc = null;
+//        try {
+//            sc = SSLContext.getInstance("SSL");
+//            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        } catch (KeyManagementException e) {
+//            e.printStackTrace();
+//        }
+//        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+//
+//        // Create all-trusting host name verifier
+//        HostnameVerifier allHostsValid = new HostnameVerifier() {
+//            public boolean verify(String hostname, SSLSession session) { return true; }
+//        };
+//        // Install the all-trusting host verifier
+//        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
         /* End of the fix*/
         //////////////////////////////////////////////////////////////////
 
